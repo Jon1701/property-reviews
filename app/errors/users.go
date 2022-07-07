b849f21964135b39195eb6b31af2a27503cf01b5
Jon Balon <development@jonbalon.com>
2022-07-07 16:55:33 -0400

    `errors` Package for User Errors

---
+++ app/errors/users.go
@@ -0,0 +1,15 @@
+package errors
+
+const (
+	UsernameMinLength int = 1
+	UsernameMaxLength int = 50
+	PasswordMinLength int = 8
+	PasswordMaxLength int = 1000
+	EmailAddressMinLength int = 5
+	EmailAddressMaxLength int = 255
+)
+
+var UsernameInvalidFieldLength ErrorMessage = FieldMustBeBetweenXandYCharactersInLength(UsernameMinLength, UsernameMaxLength)
+var PasswordInvalidFieldLength ErrorMessage = FieldMustBeBetweenXandYCharactersInLength(PasswordMinLength, PasswordMaxLength)
+var EmailAddressRequired ErrorMessage = "a valid email address is required"
+var EmailAddressInvalidFieldLength ErrorMessage = FieldMustBeBetweenXandYCharactersInLength(EmailAddressMinLength, EmailAddressMaxLength)

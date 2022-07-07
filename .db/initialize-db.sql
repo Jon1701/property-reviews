a8a31977239bc286b64486dd4ed8839a762fbce2
Jon Balon <development@jonbalon.com>
2022-07-07 12:14:53 -0400

    Create Users Table Statement in Database Initialization Script

--- .db/initialize-db.sql
+++ .db/initialize-db.sql
@@ -9,3 +9,27 @@ BEGIN
 	END IF;
 END
 $do$;
+
+/* Enable module to generate UUIDs */
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+/* Create Users table */
+CREATE TABLE IF NOT EXISTS users (
+	id						SERIAL
+								PRIMARY KEY
+								NOT NULL,
+
+	id_hash				UUID
+								NOT NULL
+								DEFAULT uuid_generate_v4(),
+
+	username			VARCHAR(50)
+								NOT NULL,
+
+	email_address	VARCHAR(255)
+								NOT NULL
+								UNIQUE,
+
+	password			TEXT
+								NOT NULL
+);

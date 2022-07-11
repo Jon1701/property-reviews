e2e06af09ce7bb0f6e404c3060f064a122bbdbf1
Jon Balon <development@jonbalon.com>
2022-07-11 16:46:36 -0400

    Update `main.go` to use `AppContext`

--- main.go
+++ main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/Jon1701/property-reviews/app/controllers"
 	"github.com/Jon1701/property-reviews/app/routes"
+	"github.com/Jon1701/property-reviews/storage"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default();
+	db, err := storage.NewConnection()
+	if err != nil {
+		log.Fatal("Failed to connect to the database", err)
+	}
 
+	appCtx := controllers.New(db)
+
+	r := gin.Default()
 	routes.RegisterRoutes(r)
 
 	api := r.Group("/api")
-
 	users := api.Group("/users")
-	routes.RegisterUserRoutes(users)
-	
+	routes.RegisterUserRoutes(&appCtx, users)
+
 	serverPort := os.Getenv("SERVER_PORT")
 
-	err := r.Run(fmt.Sprintf(":%s", serverPort))
+	err = r.Run(fmt.Sprintf(":%s", serverPort))
 	if err != nil {
 		panic(fmt.Sprintf("Failed to start the web server - Error %v", err))
 	}

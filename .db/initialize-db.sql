b89e07c32745fa1caa9d14cba5ca3d475971e516
Jon Balon <development@jonbalon.com>
2022-07-12 01:30:22 -0400

    Remove Environment Variables for Postgres Application Credentials

--- .db/initialize-db.sql
+++ .db/initialize-db.sql
@@ -1,15 +1,3 @@
--- /* Create application user */
--- DO
--- $do$
--- BEGIN
--- 	IF EXISTS (SELECT FROM pg_user WHERE usename='appuser') THEN
--- 		RAISE NOTICE 'Skipping application user creation due to existing user';
--- 	ELSE
--- 		CREATE USER appuser WITH PASSWORD 'appuser';
--- 	END IF;
--- END
--- $do$;
-
 -- /* Enable module to generate UUIDs */
 -- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 

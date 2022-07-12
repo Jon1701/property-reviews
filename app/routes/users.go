93c4b43d00df4a5ef36e7b2a7874351bb199751d
Jon Balon <development@jonbalon.com>
2022-07-12 02:07:17 -0400

    Register the `POST /api/users/login` Route

--- app/routes/users.go
+++ app/routes/users.go
@@ -8,4 +8,5 @@ import (
 // Registers the routes under the /api/users group.
 func RegisterUserRoutes(appCtx *controllers.AppContext, rg *gin.RouterGroup) {
 	rg.POST("/signup", appCtx.CreateUser)
+	rg.POST("/login", appCtx.UserLogin)
 }

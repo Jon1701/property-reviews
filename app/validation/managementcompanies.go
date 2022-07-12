4e3497bf12cacb46363a3081e3c5d6907ba6def1
Jon Balon <development@jonbalon.com>
2022-07-12 12:42:58 -0400

    Validation Function for Create Management Company

---
+++ app/validation/managementcompanies.go
@@ -0,0 +1,67 @@
+package validation
+
+import (
+	"github.com/Jon1701/property-reviews/app/errormessages"
+	"github.com/Jon1701/property-reviews/app/serializers"
+)
+
+type Address struct {
+	Line1      *errormessages.ErrorMessage `json:"line1,omitempty"`
+	Line2      *errormessages.ErrorMessage `json:"line2,omitempty"`
+	City       *errormessages.ErrorMessage `json:"city,omitempty"`
+	State      *errormessages.ErrorMessage `json:"state,omitempty"`
+	PostalCode *errormessages.ErrorMessage `json:"postalCode,omitempty"`
+	Country    *errormessages.ErrorMessage `json:"country,omitempty"`
+}
+
+type ManagementCompany struct {
+	Name    *errormessages.ErrorMessage `json:"name,omitempty"`
+	Address *Address                    `json:"password,omitempty"`
+	Website *errormessages.ErrorMessage `json:"website,omitempty"`
+}
+
+// Performs field validation for Management Company struct values under the
+// Create Management Company route.
+func ValidateCreateManagementCompany(company serializers.ManagementCompany) *ManagementCompany {
+	results := ManagementCompany{}
+	passValidation := true
+
+	// Check Name length.
+	if company.Name == nil {
+		msg := errormessages.FieldValueRequired
+		results.Name = &msg
+		passValidation = false
+	} else {
+		isLengthValid := len(*company.Name) >= errormessages.ManagementCompanyNameMinLength || len(*company.Name) <= errormessages.ManagementCompanyNameMinLength
+		if !isLengthValid {
+			results.Name = &errormessages.ManagementCompanyNameInvalidFieldLength
+			passValidation = false
+		}
+	}
+
+	// Check Address validity.
+	resultsAddress := ValidateAddress(company.Address, false)
+	if resultsAddress != nil {
+		results.Address = resultsAddress
+		passValidation = false
+	}
+
+	// Check Website URL validity and length.
+	if company.Website != nil {
+		if !IsValidURL(*company.Website) {
+			results.Website = &errormessages.InvalidURL
+			passValidation = false
+		}
+
+		if !IsURLOfCorrectLength(*company.Website) {
+			results.Website = &errormessages.WebsiteInvalidLength
+			passValidation = false
+		}
+	}
+
+	if passValidation {
+		return nil
+	}
+
+	return &results
+}

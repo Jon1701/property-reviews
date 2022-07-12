fa3129d9139f86d7df2bb7ad30ff238d1fe5a392
Jon Balon <development@jonbalon.com>
2022-07-12 13:33:51 -0400

    Create Management Companies Table

--- .db/initialize-db.sql
+++ .db/initialize-db.sql
@@ -16,3 +16,36 @@ CREATE TABLE IF NOT EXISTS users (
 	password			TEXT
 								NOT NULL
 );
+
+/* Create Management Companies table */
+CREATE TABLE IF NOT EXISTS management_companies (
+	id									SERIAL
+											PRIMARY KEY
+											NOT NULL,
+
+	id_hash							CHAR(50)
+											UNIQUE
+											NOT NULL,
+
+	name								VARCHAR(1000)
+											NOT NULL,
+	
+	address_line1				VARCHAR(1000)
+											NOT NULL,
+
+	address_line2				VARCHAR(1000),
+
+	address_city				VARCHAR(1000)
+											NOT NULL,
+
+	address_state				VARCHAR(1000)
+											NOT NULL,
+
+	address_postal_code	VARCHAR(20)
+											NOT NULL,
+
+	address_country			VARCHAR(100)
+											NOT NULL,
+	
+	website							VARCHAR(255)
+) 

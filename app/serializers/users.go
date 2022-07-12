4947bf95055199700ac79d9cbcae1c1a3871e28c
Jon Balon <development@jonbalon.com>
2022-07-12 11:30:11 -0400

    Serializers for Address and Management Company

--- app/serializers/users.go
+++ app/serializers/users.go
@@ -5,3 +5,19 @@ type User struct {
 	Password     *string `json:"password,omitempty"`
 	EmailAddress *string `json:"emailAddress,omitempty"`
 }
+
+type Address struct {
+	Line1      *string `json:"line1,omitempty"`
+	Line2      *string `json:"line2,omitempty"`
+	City       *string `json:"city,omitempty"`
+	State      *string `json:"state,omitempty"`
+	PostalCode *string `json:"postalCode,omitempty"`
+	Country    *string `json:"country,omitempty"`
+}
+
+type ManagementCompany struct {
+	ID      *string  `json:"id,omitempty"`
+	Name    *string  `json:"name,omitempty"`
+	Address *Address `json:"address,omitempty"`
+	Website *string  `json:"website,omitempty"`
+}

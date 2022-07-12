e78b20614d647c43c3e8dd043897294394b790dc
Jon Balon <development@jonbalon.com>
2022-07-12 12:44:44 -0400

    Register the `POST /api/management` Route

---
+++ app/routes/managementcompanies.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/Jon1701/property-reviews/app/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+// Registers the routes under the /api/management group.
+func RegisterManagementCompaniesRoutes(appCtx *controllers.AppContext, rg *gin.RouterGroup) {
+	rg.POST("/", appCtx.CreateManagementCompany)
+}

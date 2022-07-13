c0c2a70781b15b1d98cbcc0fd5d06dc638148787
Jon Balon <development@jonbalon.com>
2022-07-13 12:05:20 -0400

    Move `Address` Serializer Struct to `serializers/common.go`

--- app/serializers/users.go
+++ app/serializers/users.go
@@ -5,12 +5,3 @@ type User struct {
 	Password     *string `json:"password,omitempty"`
 	EmailAddress *string `json:"emailAddress,omitempty"`
 }
-
-type Address struct {
-	Line1      *string `json:"line1,omitempty"`
-	Line2      *string `json:"line2,omitempty"`
-	City       *string `json:"city,omitempty"`
-	State      *string `json:"state,omitempty"`
-	PostalCode *string `json:"postalCode,omitempty"`
-	Country    *string `json:"country,omitempty"`
-}

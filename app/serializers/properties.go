78c177800d57751524b2048ff0ba791c6ad0e3a9
Jon Balon <development@jonbalon.com>
2022-07-13 13:25:32 -0400

    Define `Property` Serializer

---
+++ app/serializers/properties.go
@@ -0,0 +1,29 @@
+package serializers
+
+type PropertyType string
+type BuildingType string
+
+var TypesOfProperties = map[string]PropertyType{
+	"CommercialProperty":  "commercial",
+	"ResidentialProperty": "residential",
+}
+
+var TypesOfBuildings = map[string]BuildingType{
+	"Apartment":     "apartment",
+	"BasementSuite": "basement-suite",
+	"Bungalow":      "bungalow",
+	"Cabin":         "cabin",
+	"Condominium":   "condominium",
+	"CoOp":          "co-op",
+	"Cottage":       "cottage",
+	"DetachedHouse": "detached",
+	"Townhome":      "townhome",
+}
+
+type Property struct {
+	ID           *string       `json:"id,omitempty"`
+	Address      *Address      `json:"address,omitempty"`
+	Neighborhood *string       `json:"neighborhood,omitempty"`
+	PropertyType *PropertyType `json:"propertyType,omitempty"`
+	BuildingType *BuildingType `json:"buildingType,omitempty"`
+}
